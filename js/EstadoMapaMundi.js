f4928591fb3a1ac0d9635b43a25dedd776ab207d
Samuel <samuelvillegas04@gmail.com>
2019-03-19 17:26:31 -0400

    el problema es la imagen en sí

--- js/EstadoMapaMundi.js
+++ js/EstadoMapaMundi.js
@@ -1,30 +1,30 @@
 function EstadoMapaMundi(){
-  var that = this;
-  this.mapaListo = false;
-  this.mapa = null;
-  this.jugadorMapamundi = null;
-  ajax.cargarArchivo("mapas/arkanos.json", function(objetoJSON){
+    var that = this;
+    this.mapaListo = false;
+    this.mapa = null;
+    this.jugadorMapamundi = null;
+    ajax.cargarArchivo("mapas/desierto48.json", function(objetoJSON){
 
-    that.mapa = new Mapa(objetoJSON);
+        that.mapa = new Mapa(objetoJSON);
 
-    that.mapaListo = true;
-    that.jugadorMapamundi = new JugadorMapamundi(new Punto(0,0));
-    console.log("Mapa ready");
-  });
+        that.mapaListo = true;
+        that.jugadorMapamundi = new JugadorMapamundi(new Punto(500,500));
+        console.log("Mapa ready");
+    });
 
 }
 
 EstadoMapaMundi.prototype.actualizar = function (registroTemporal) {
-  if(!this.mapaListo){
-    return;
-  }
-  this.jugadorMapamundi.actualizar(registroTemporal);
-  this.mapa.actualizar(registroTemporal, this.jugadorMapamundi.posicionEnMapaEnPixeles);
+    if(!this.mapaListo){
+        return;
+    }
+    this.jugadorMapamundi.actualizar(registroTemporal);
+    this.mapa.actualizar(registroTemporal, this.jugadorMapamundi.posicionEnMapaEnPixeles);
 };
 
 EstadoMapaMundi.prototype.dibujar = function () {
-if(!this.mapaListo){
-  return;
-}
-this.mapa.dibujar();
+    if(!this.mapaListo){
+        return;
+    }
+    this.mapa.dibujar();
 };
--- js/jugadorMapamundi.js
+++ js/jugadorMapamundi.js
@@ -1,45 +1,48 @@
 function JugadorMapamundi(posicionInicialEnPixeles){
-  this.ancho = 16; //tamaño de los sprites
-  this.alto = 32;
+    this.ancho = 16; //tamaño de los sprites
+    this.alto = 32;
 
-  this.velocidadMovimiento = 10;
+    this.velocidadMovimiento = 5;
 
-  var centroX = dimensiones.ancho / 2 - this.ancho / 2;
-  var centroY = dimensiones.alto / 2 - this.alto / 2;
+    var centroX = Math.trunc(dimensiones.ancho / 2 - this.ancho / 2);
+    var centroY = Math.trunc(dimensiones.alto / 2 - this.alto / 2);
 
-  this.posicionCentrada = new Punto(centroX, centroY);
+    this.posicionCentrada = new Punto(centroX, centroY);
 
-  this.posicionEnMapaEnPixeles = posicionInicialEnPixeles;
+    posicionInicialEnPixeles.x *= -1;
+    posicionInicialEnPixeles.y *= -1;
 
-   this.aplicarEstilos();
+    this.posicionEnMapaEnPixeles = new Punto(this.posicionCentrada.x + posicionInicialEnPixeles.x,
+        this.posicionCentrada.y + posicionInicialEnPixeles.y);
+    this.aplicarEstilos();
 }
 
 JugadorMapamundi.prototype.aplicarEstilos = function () {
-  var idHTML = "jugador";
+    var idHTML = "jugador";
 
-  document.getElementById(idHTML).style.background = "blue";
-  document.getElementById(idHTML).style.position = "absolute";
-  document.getElementById(idHTML).style.width = this.ancho + "px";
-  document.getElementById(idHTML).style.height = this.alto + "px";
-  document.getElementById(idHTML).style.zIndex = "10";
-  document.getElementById(idHTML).style.left = this.posicionCentrada.x + "px";
-  document.getElementById(idHTML).style.top = this.posicionCentrada.y + "px";
+    document.getElementById(idHTML).style.background = "blue";
+    document.getElementById(idHTML).style.position = "absolute";
+    document.getElementById(idHTML).style.width = this.ancho + "px";
+    document.getElementById(idHTML).style.height = this.alto + "px";
+    document.getElementById(idHTML).style.zIndex = "10";
+    document.getElementById(idHTML).style.left = this.posicionCentrada.x + "px";
+    document.getElementById(idHTML).style.top = this.posicionCentrada.y + "px";
 };
 
 JugadorMapamundi.prototype.actualizar = function (registroTemporal) {
-  if(teclado.teclaPulsada(controlesTeclado.arriba)){
-    this.posicionEnMapaEnPixeles.y += this.velocidadMovimiento;
-  }
+    if(teclado.teclaPulsada(controlesTeclado.arriba)){
+        this.posicionEnMapaEnPixeles.y += this.velocidadMovimiento;
+    }
 
-  if(teclado.teclaPulsada(controlesTeclado.abajo)){
-    this.posicionEnMapaEnPixeles.y -= this.velocidadMovimiento;
-  }
+    if(teclado.teclaPulsada(controlesTeclado.abajo)){
+        this.posicionEnMapaEnPixeles.y -= this.velocidadMovimiento;
+    }
 
-  if(teclado.teclaPulsada(controlesTeclado.izquierda)){
-    this.posicionEnMapaEnPixeles.x += this.velocidadMovimiento;
-  }
+    if(teclado.teclaPulsada(controlesTeclado.izquierda)){
+        this.posicionEnMapaEnPixeles.x += this.velocidadMovimiento;
+    }
 
-  if(teclado.teclaPulsada(controlesTeclado.derecha)){
-    this.posicionEnMapaEnPixeles.x -= this.velocidadMovimiento;
-  }
+    if(teclado.teclaPulsada(controlesTeclado.derecha)){
+        this.posicionEnMapaEnPixeles.x -= this.velocidadMovimiento;
+    }
 };
